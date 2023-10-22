e1f36e2be5a3e26d6ae9f00112e66819d35dd4da
Bodgan B Peterson <github@bogdanbpeterson.com>
2023-10-22 20:51:48 +0300

    actually fixes stuff

--- sripts/rock-paper-scssors.js
+++ sripts/rock-paper-scssors.js
@@ -16,27 +16,27 @@ function playGame(playerMove) {
   let result = "";
 
   if (playerMove === "Scissors") {
-    if (computerMove === "Rock") {
+    if (computerMove === "rock") {
       result = "You Lose.";
-    } else if (computerMove === "Paper") {
+    } else if (computerMove === "paper") {
       result = "You Win.";
-    } else if (computerMove === "Scissors") {
+    } else if (computerMove === "scissors") {
       result = "Tie.";
     }
-  } else if (playerMove === "Paper") {
-    if (computerMove === "Rock") {
+  } else if (playerMove === "paper") {
+    if (computerMove === "rock") {
       result = "You Win.";
-    } else if (computerMove === "Paper") {
+    } else if (computerMove === "paper") {
       result = "Tie.";
-    } else if (computerMove === "Scissors") {
+    } else if (computerMove === "scissors") {
       result = "You Lose.";
     }
-  } else if (playerMove === "Rock") {
-    if (computerMove === "Rock") {
+  } else if (playerMove === "rock") {
+    if (computerMove === "rock") {
       result = "Tie.";
-    } else if (computerMove === "Paper") {
+    } else if (computerMove === "paper") {
       result = "You lose.";
-    } else if (computerMove === "Scissors") {
+    } else if (computerMove === "scissors") {
       result = "You Win.";
     }
   }
@@ -55,6 +55,7 @@ function playGame(playerMove) {
 
   document.querySelector(".js-result").innerHTML = result;
 
+  console.log(playerMove);
   document.querySelector(".js-moves").innerHTML = `You 
 <img src="./images/${playerMove}-emoji.png" class="move-icon">
 <img src="./images/${computerMove}-emoji.png" class="move-icon">
